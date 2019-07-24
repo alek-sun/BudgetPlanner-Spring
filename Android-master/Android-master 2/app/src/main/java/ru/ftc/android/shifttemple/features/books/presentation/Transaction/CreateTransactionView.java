74c7526e08d183ac5e2b033bcefe26cd8abb544a
Ирина Федяева <i.fediaeva@g.nsu.ru>
2019-07-24 23:24:56 +0700

    Deleted useless files

--- Android-master/Android-master 2/app/src/main/java/ru/ftc/android/shifttemple/features/books/presentation/Transaction/CreateTransactionView.java
+++
@@ -1,9 +0,0 @@
-package ru.ftc.android.shifttemple.features.books.presentation.Transaction;
-
-import ru.ftc.android.shifttemple.features.MvpView;
-
-public interface CreateTransactionView extends MvpView {
-    void onTransactionCreated();
-
-    void showError(String message);
-}
